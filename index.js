c38a39d6332c291cf92f5950f08d9719651b2680
Joshua Bellamy <github@smysnk.com>
2026-03-18 18:34:10 -0400

    Use mounted JustinSDK and NopSCAD sources

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 import * as modeling from "@jscad/modeling";
-import { createJustinShape } from "/Libraries/JustinSDK";
-import { createNopShape } from "varcad:library/nopscad";
+import * as roundedCubeLib from "/Libraries/JustinSDK/src/rounded_cube.scad?use";
+import * as roundedCylinderLib from "/Libraries/NopSCAD/utils/rounded_cylinder.scad?use";
 
 export function main() {
   return modeling.booleans.union(
-    createJustinShape(),
-    modeling.transforms.translate([0.4, 1.15, 0], createNopShape()),
+    roundedCubeLib.rounded_cube([1.7, 1.05, 0.65], 0.16, true),
+    modeling.transforms.translate([0.45, 1.1, 0], roundedCylinderLib.rounded_cylinder(0.45, 0.95, 0.12)),
     modeling.transforms.translate(
       [-0.6, -0.25, 0.3],
       modeling.primitives.sphere({ radius: 0.55, segments: 24 }),
